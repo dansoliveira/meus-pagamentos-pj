aaa59fc06cacbbd7b59c9344d7218d2a2ac1f768
Daniel Oliveira <oliveiradaniel@protonmail.com>
2021-12-15 09:59:25 -0300

    feat: add mock calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const numberFormat = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+
 function App() {
+  const [value, setValue] = React.useState<string>('')
+  const [hours, setHours] = React.useState<number>(0)
+  const [minutes, setMinutes] = React.useState<number>(0)
+  const valorHora = 40
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value
+    const splittedTime = value.split(' ')
+    const hours = splittedTime.reduce((prev, cur) => {
+        const [hours, ] = cur.split(':')
+
+        return prev + Number(hours)
+    }, 0)
+
+    const minutes = splittedTime.reduce((prev, cur) => {
+        const [, minutes] = cur.split(':')
+
+        return prev + Number(minutes)
+    }, 0)
+
+    setValue(value)
+    setHours(isNaN(hours) ? 0 : hours)
+    setMinutes(isNaN(minutes) ? 0 : minutes)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="textarea" value={value} onChange={handleInputChange} />
+      <h5>Valor hora</h5>
+      <p>{numberFormat.format(valorHora)}</p>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '2rem',
+      }}>
+        <div>
+          <h5>Horas</h5>
+          <p>{hours}</p>
+        </div>
+        <div>
+          <h5>Minutos</h5>
+          <p>{minutes}</p>
+        </div>
+      </div>
+      <h4>Valor à receber</h4>
+      <p>{numberFormat.format(hours * valorHora)}</p>
     </div>
   );
 }
